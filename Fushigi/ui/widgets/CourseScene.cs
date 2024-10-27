50c41084b75508dee22ee7f8179fa1ad89c2a74f
Donavin Draws <51259260+DonavinDraws@users.noreply.github.com>
2024-10-26 21:46:34 -0500

    Update CourseScene.cs

--- Fushigi/ui/widgets/CourseScene.cs
+++ Fushigi/ui/widgets/CourseScene.cs
@@ -502,7 +502,7 @@ namespace Fushigi.ui.widgets
         
         public void Save()
         {
-            string[] sizeTables = Directory.GetFiles("System", "Resource");
+            string[] sizeTables = Directory.GetFiles(Path.Combine("System", "Resource"));
             foreach (string path in sizeTables)
             {
                 RSTB resource_table = new RSTB();
